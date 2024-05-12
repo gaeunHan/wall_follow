805b05b395c1d1291409694f74409a8213836a16
ghan <hge4275@naver.com>
2024-05-12 17:07:54 +0900

    seems working..

--- wall_following_pkg/wall_following_pkg/wall_follow_node.py
+++ wall_following_pkg/wall_following_pkg/wall_follow_node.py
@@ -36,7 +36,7 @@ class WallFollow(Node):
         '''
         
         # create serial object
-        self.serial = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+        self.ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 
         # set PID gains
         self.kp = 14
@@ -57,8 +57,10 @@ class WallFollow(Node):
         self.L_lookAhead = 1.5          # meters
         self.throttle_max = 60          # fastest throttle
         self.steering_max = 1           # rightest steering
-        self.LiDAR_detect_max = 10      # meters
-        self.refDistToRightWall = 0.5   # meters
+        self.LiDAR_detect_max = 16      # meters
+        self.refDistToRightWall = 0.2   # meters
+        self.distPoint1 = -55           # degree
+        self.distPoint2 = -90           # degree
 
     def toRadian(self, degree):
         """
@@ -78,16 +80,20 @@ class WallFollow(Node):
         if(math.isinf(distance) or math.isnan(distance)):
             distance = self.LiDAR_detect_max
 
+        print("dist from ", round(angle/math.pi * 180), ": ", round(distance,2))
         return distance
 
     def get_error(self, a, b, theta):
         """
         Calculates the error to the wall. Follow the wall on the right
         """
+        theta = self.toRadian(theta)
         alpha = math.atan((a * math.cos(theta) - b)/(a*math.sin(theta)))
         currDistToRightWall = b*math.cos(alpha)
 
         currErr = self.refDistToRightWall - currDistToRightWall
+        print("theta: ", round(theta/math.pi * 180), " alpha: ", round(alpha/math.pi * 180))
+        print("curr dist: ", currDistToRightWall, " dist err: ", currErr)
 
         # advanced
         aheadDistToRightWall = currDistToRightWall + self.L_lookAhead * math.sin(alpha)
@@ -99,51 +105,79 @@ class WallFollow(Node):
         Based on the calculated error, publish vehicle control
         """
 
-        error = self.get_error()
         self.integral = self.integral + error
 
         # make a steering control input
-        angle = self.kp * error + self.kd * (error - self.prev_error) + self.ki * self.integral
-
+        PIDdist = (self.kp * error + self.kd * (error - self.prev_error) + self.ki * self.integral)
+        print("PID calculated dist: ", round(PIDdist,2))
         self.prev_error = error 
 
+        max_PIDdist = self.kp * self.refDistToRightWall
+        min_PIDdist = self.kp * (self.refDistToRightWall - self.LiDAR_detect_max*math.cos(math.atan((self.LiDAR_detect_max * math.cos(abs(self.distPoint1 - self.distPoint2)) - self.LiDAR_detect_max)/(self.LiDAR_detect_max*math.sin(abs(self.distPoint1 - self.distPoint2))))))     
+            
+        print("max PIDdist: ", max_PIDdist, " min PIDdist: ", min_PIDdist) 
+        steeringPWM = self.calculate_pwm(PIDdist, max_PIDdist, min_PIDdist)
+        print("steeringPWM: ", steeringPWM)
+        
         # determine throttle speed depends on the amount of steering control input 
-        if angle >= abs(self.toRadian(0)) and angle < abs(self.toRadian(10)):
-            velocity = self.throttle_max
-        elif angle >= abs(self.toRadian(10)) and angle < abs(self.toRadian(20)):
-            velocity = self.throttle_max / 2
+        if PIDdist >= abs(self.toRadian(0)) and PIDdist < abs(self.toRadian(10)):
+            velocityPWM = 140 #self.throttle_max
+        elif PIDdist >= abs(self.toRadian(10)) and PIDdist < abs(self.toRadian(20)):
+            velocityPWM = 100 #self.throttle_max / 2
         else:
-            velocity = self.throttle_max / 4
+            velocityPWM = 60 #self.throttle_max / 4
 
         '''
         drive_msg = AckermannDriveStamped()
-        drive_msg.steering_angle = angle
+        drive_msg.steering_angle = PIDdist
         drive_msg.speed = velocity
         self.publisher.publish(drive_msg)
         '''
 
-        return angle, velocity
+        return int(steeringPWM), int(0)
+        
+    def calculate_pwm(self, PIDdist, max_PIDdist, min_PIDdist):
+        max_pwm = 140    # max turn right
+        min_pwm = 60     # max turn left
+        mid_pwm = 100    # go straight
+         
+        # go left
+        if(PIDdist > 0):
+            pwm = -((mid_pwm - min_pwm) / max_PIDdist) * PIDdist + mid_pwm      
+            print("=== go left ===")  
+        # go right
+        elif(PIDdist <= 0):
+            pwm = ((max_pwm - mid_pwm) / -max_PIDdist) * PIDdist + mid_pwm   
+            print("=== go right ===")
+        
+        print("pwm w/o saturation: ", pwm)
+
+        # saturation        
+        if(pwm < 60): pwm = min_pwm
+        elif(pwm > 140): pwm = max_pwm
+        
+        return int(pwm)
     
     def send_control_to_car(self, steering, throttle):
-        self.ser.write(bytearray([throttle, steering])) # throttle, steering 값을 serial 통신을 통해 보냅니다.
-        self.get_logger().info('Sending: Throttle=%d, Steer=%d' % (throttle, steering)) # throttle, steering 값을 로그에 출력합니다.
+        self.ser.write(bytearray([throttle, steering])) # send throttle, steering throguth serial com.
+        self.get_logger().info('Sending pwm: Throttle=%d, Steer=%d\n' % (throttle, steering))
 
     def scan_callback(self, msg):
         """
         Callback function for LaserScan messages. Calculate the error and publish the drive message in this function.
         """
-        a = self.get_range(self.toRadian(msg, -55.0))
-        b = self.get_range(self.toRadian(msg, -90.0))
-        theta = 90.0 - 55.0
+        a = self.get_range(msg, self.toRadian(self.distPoint1))
+        b = self.get_range(msg, self.toRadian(self.distPoint2))
+        theta = abs(self.distPoint1 - self.distPoint2)
 
         # calc dist error
         error = self.get_error(a, b, theta)
 
         # make control input
-        angle, velocity = self.pid_control(error) 
+        PIDdist, velocity = self.pid_control(error) 
 
         # drive the car 
-        self.send_control_to_car(angle, velocity)
+        self.send_control_to_car(PIDdist, velocity)
 
         
 
